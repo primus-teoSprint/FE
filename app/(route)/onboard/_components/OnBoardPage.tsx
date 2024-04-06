5c5cda8bd8b96a206fcd25b5b0dc2c6c474a5c66
chaduhwan <enghks2973@naver.com>
2024-04-06 19:53:55 +0900

    [#5]온보딩페이지 ui 구현 및 애니메이션 구현

---
+++ app/(route)/onboard/_components/OnBoardPage.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useState } from 'react';
+import Onboarding from "../_components/OnBoarding";
+import WatchWord from "../_components/WatchWord";
+import styles from '../Onboard.module.css';
+
+function OnboardPage() {
+  const [showWatchWord, setShowWatchWord] = useState(true);
+
+
+  return (
+    <div className={styles.background}>
+      {showWatchWord && <WatchWord handleView={setShowWatchWord}/>}
+      {!showWatchWord && <Onboarding />}
+    </div>
+  );
+}
+
+export default OnboardPage;
