d8a78e687f602a5e69ac676f308f41a09fe6e854
03hoho03 <ghwjd321@naver.com>
2024-04-06 19:39:51 +0900

    [#3] suspense 추가

--- app/(route)/verification/ibulsin/page.tsx
+++ app/(route)/verification/ibulsin/page.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Step from './_components/Step'
 
 function page() {
   return (
     <div>
-      <Step />
+      <Suspense fallback={<div>loading...</div>}>
+        <Step />
+      </Suspense>
     </div>
   )
 }
