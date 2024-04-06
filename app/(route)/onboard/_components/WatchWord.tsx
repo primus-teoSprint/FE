80e2813a3febf9cb06c3f02bd9e7c6de00e40d8e
chaduhwan <enghks2973@naver.com>
2024-04-06 20:21:24 +0900

    [fix]프리티어적용

--- app/(route)/onboard/_components/WatchWord.tsx
+++ app/(route)/onboard/_components/WatchWord.tsx
@@ -1,26 +1,26 @@
-import { useEffect,useRef } from 'react';
-import styles from '../Onboard.module.css';
+import { useEffect, useRef } from 'react'
+import styles from '../Onboard.module.css'
 
-function WatchWord ({ handleView }: { handleView: any }) {
-    const animationRef = useRef<HTMLDivElement>(null);
+function WatchWord({ handleView }: { handleView: any }) {
+  const animationRef = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
-        const animationend = () => {
-            handleView(false)
-        }
-    
-        animationRef.current?.addEventListener('animationend', animationend);
-    
-        return () => {
-          animationRef.current?.removeEventListener('animationend', animationend);
-        };
-      }, []);
-    return (
-        <div className={styles.wordwrap} ref={animationRef}>
-            <p className={styles.word}>당신의 아이디어가</p>
-            <p className={styles.word}>로켓처럼 성장하도록</p>
-        </div>
-    );
+  useEffect(() => {
+    const animationend = () => {
+      handleView(false)
+    }
+
+    animationRef.current?.addEventListener('animationend', animationend)
+
+    return () => {
+      animationRef.current?.removeEventListener('animationend', animationend)
+    }
+  }, [])
+  return (
+    <div className={styles.wordwrap} ref={animationRef}>
+      <p className={styles.word}>당신의 아이디어가</p>
+      <p className={styles.word}>로켓처럼 성장하도록</p>
+    </div>
+  )
 }
 
-export default WatchWord;
+export default WatchWord
