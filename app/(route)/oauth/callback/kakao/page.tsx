db4d9b1cafe6ec58218230603eacccb0299702a1
03hoho03 <ghwjd321@naver.com>
2024-04-07 18:54:33 +0900

    [#26] 카카오 소셜로그인 redirect 페이지

---
+++ app/(route)/oauth/callback/kakao/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Callback from './_components/callback'
+
+const page = () => {
+  return (
+    <div>
+      <Callback />
+    </div>
+  )
+}
+
+export default page
