c4920a962aaeb07049cdde4538358296ce49004c
chaduhwan <enghks2973@naver.com>
2024-04-08 01:11:13 +0900

    [#31] 메인페이지 api 연결

--- app/(route)/main/page.tsx
+++ app/(route)/main/page.tsx
@@ -5,9 +5,27 @@ import Nav from '../../_common/nav/index'
 import Title from '../../_common/text/title'
 import Search from '../../_components/search'
 import S from './page.module.css'
+import { useQuery } from '@tanstack/react-query'
+import axios, { AxiosResponse } from 'axios'
+import { Data, Tool, Root } from '../../_constants/toolcard'
+import ToolCard from '@/app/_components/card/toolCard'
+import KeyWordTool from '@/app/_components/card/toolCard/keyWordToolCard'
 
 //TODO: 주석처리 사항들에 데이터 가져오기
 function Main() {
+  const { data, isFetching } = useQuery<AxiosResponse<Root>>({
+    queryKey: ['getTools'],
+    queryFn: () => axios.get('http://222.121.148.192/api/toolDetails'),
+  })
+
+  console.log(data)
+
+  if (isFetching) {
+    return <div>...loading</div>
+  }
+
+  const resData = data?.data.data.tool
+
   return (
     <main className={S.wrapper}>
       <Nav />
@@ -19,19 +37,35 @@ function Main() {
           <Title title="인기 많은 툴" />
         </div>
         <div className={S.toolCardWrapper}>
-          {/* <ToolCard
-            title=""
-            subTitle=""
-            description=""
-            toolImg=""
-          /> */}
+          {resData?.map((data: Tool, index: number) => {
+            return (
+              <ToolCard
+                title={data?.title || ''}
+                subTitle={data?.subTitle || ''}
+                description={data?.description || ''}
+                toolImg={data?.toolImg || ''}
+                toolId={index}
+                key={index}
+              />
+            )
+          })}
         </div>
         <div className={S.rowWrapper}>
           <Title title="검증 툴 리스트" />
           <Link href="/list">자세히 보기</Link>
         </div>
         <div className={S.keyWordCardWrapper}>
-          {/* <KeyWordTool title="" subTitle="" toolImg="" /> */}
+          {resData?.map((data: Tool, index) => {
+            return (
+              <KeyWordTool
+                title={data?.title || ''}
+                subTitle={data?.subTitle || ''}
+                toolImg={data?.toolImg || ''}
+                key={index}
+                toolId={index}
+              />
+            )
+          })}
         </div>
       </div>
     </main>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'github.com',
+      'stlab-storage-main.s3.ap-northeast-2.amazonaws.com',
+    ], // 이미지 호스트를 여기에 추가합니다.
+  },
+}
 
 export default nextConfig
