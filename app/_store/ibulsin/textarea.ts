25fcd7e4d8c37cd4627b9a4fa631e4c94911b48c
03hoho03 <ghwjd321@naver.com>
2024-04-07 23:54:49 +0900

    [#32] textarea recoil value

---
+++ app/_store/ibulsin/textarea.ts
@@ -0,0 +1,31 @@
+import { atom } from 'recoil'
+
+export const outlineTextareaAtom = atom({
+  key: 'outlineTextareaAtom',
+  default: '',
+})
+
+export const whyTextareaAtom = atom({
+  key: 'whyTextareaAtom',
+  default: '',
+})
+
+export const marketResponseTextareaAtom = atom({
+  key: 'marketResponseTextareaAtom',
+  default: '',
+})
+
+export const XYZTextareaAtom = atom({
+  key: 'XYZTextareaAtom',
+  default: '',
+})
+
+export const xyzTextareaAtom = atom({
+  key: 'xyzTextareaAtom',
+  default: '',
+})
+
+export const pretotypingTextareaAtom = atom({
+  key: 'pretotypingTextareaAtom',
+  default: '',
+})
