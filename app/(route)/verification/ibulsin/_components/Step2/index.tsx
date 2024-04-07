3c8e65442a74e94d8ad12b723240709e657578e3
03hoho03 <ghwjd321@naver.com>
2024-04-07 23:54:17 +0900

    [#32] props 구조 변경 적용

--- app/(route)/verification/ibulsin/_components/Step2/index.tsx
+++ app/(route)/verification/ibulsin/_components/Step2/index.tsx
@@ -5,8 +5,15 @@ import S from './index.module.css'
 import Link from 'next/link'
 import Textarea from '../Textarea'
 import MoreExplainBtn from '../MoreExplainBtn'
+import { ibulsinVariants } from '@/app/_constants/ibulsin'
+import {
+  XYZTextareaAtom,
+  marketResponseTextareaAtom,
+} from '@/app/_store/ibulsin/textarea'
 
 function Step2() {
+  const { marketResponse, XYZ } = ibulsinVariants
+
   return (
     <div className={S.main_container}>
       <div className={S.step_info}>
@@ -28,7 +35,10 @@ function Step2() {
             아이디어를 어떻게 받아들일지에 대한 우리의 핵심 신념이나 가정을
             얘기해요.
           </p>
-          <Textarea fieldKey={'marketResponse'} />
+          <Textarea
+            fieldKey={marketResponse}
+            atom={marketResponseTextareaAtom}
+          />
         </div>
         <div className={S.input_container}>
           <div className={S.input_title_container}>
@@ -39,7 +49,7 @@ function Step2() {
             적어도 X퍼센트의 Y는 Z 할 것이다 라는 형태의 가설이에요. 시장호응
             가설을 바탕으로 숫자로 치환에 좀 더 치밀하게 하는 도구예요
           </p>
-          <Textarea fieldKey={'XYZ'} />
+          <Textarea fieldKey={XYZ} atom={XYZTextareaAtom} />
         </div>
         <div className={S.direction_btns}>
           <Link className={S.prev_btn} href={'/verification/ibulsin?step=1'}>
