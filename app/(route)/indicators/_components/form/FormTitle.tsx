7238ae9c8d4ac153627f79e1155acc92443c8c07
hanseulhee <3021062@gmail.com>
2024-04-07 15:08:21 +0900

    [#21] 마이페이지 개발

--- app/(route)/indicators/_components/form/FormTitle.tsx
+++ app/(route)/indicators/_components/form/FormTitle.tsx
@@ -5,7 +5,7 @@ interface Props {
   subTitle?: string
 }
 
-function Form({
+function FormTitle({
   title,
   subTitle = '해당 항목의 수치는 어떻게 되나요?',
 }: Props) {
@@ -13,12 +13,8 @@ function Form({
     <div className={S.wrapper}>
       <span className={S.title}>{title}</span>
       <p className={S.subTitle}>{subTitle}</p>
-      <div className={S.inputWrapper}>
-        <input className={S.input} placeholder="수치를 입력해 주세요." />
-      </div>
-      <span className={S.error}>숫자만 입력 가능합니다.</span>
     </div>
   )
 }
 
-export default Form
+export default FormTitle
--- app/(route)/indicators/page.tsx
+++ app/(route)/indicators/page.tsx
@@ -1,14 +1,36 @@
 'use client'
 
 import Title from '@/app/_common/text/title'
-import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import Nav from '../list/_components/nav'
-import Form from './_components/form'
+import FormTitle from './_components/form/FormTitle'
+import FormS from './_components/form/form.module.css'
 import S from './indicators.module.css'
 
 //* 투자 지표 입력 페이지입니다.
 //TODO: title에 사용자가 입력했던 요소들이 보여지게 됩니다.
+//TODO: recoil 사용
+//? 피기님이 하신 투자지표 툴로 변경 예정
 function Indicators() {
+  const router = useRouter()
+  const [inputValue, setInputValue] = useState('')
+  const [error, setError] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+    setError('')
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!/^\d+$/.test(inputValue)) {
+      setError('숫자만 입력 가능합니다.')
+    } else {
+      router.push('/result')
+    }
+  }
+
   return (
     <>
       <Nav />
@@ -16,18 +38,30 @@ function Indicators() {
         <div className={S.paddingWrapper}>
           <Title title="아이디어 제목" />
         </div>
-        <form className={S.formWrapper}>
+        <form className={S.formWrapper} onSubmit={handleSubmit}>
           <div className={S.overflowWrapper}>
-            <Form
+            <FormTitle
               title="전체 이용자 수"
               subTitle="전체 이용자 수를 가지고 총 결과 지표가 계산됩니다."
             />
-            {/* <Form title="이메일" /> */}
+
+            <div className={FormS.inputWrapper}>
+              <input
+                className={FormS.input}
+                placeholder="수치를 입력해 주세요."
+                value={inputValue}
+                onChange={handleChange}
+              />
+            </div>
+
+            {error && (
+              <span className={FormS.error}>숫자만 입력 가능합니다.</span>
+            )}
           </div>
 
           <div className={S.bottomWrapper}>
             <button type="submit" className={S.submitBtnWrapper}>
-              <Link href="/result">제출하기</Link>
+              제출하기
             </button>
           </div>
         </form>
