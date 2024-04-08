939dc222d4bd51437515b48dddc00b5962d201e3
03hoho03 <ghwjd321@naver.com>
2024-04-08 20:26:34 +0900

    [#52] 로그인 로그아웃 버튼 추가

--- app/_common/nav/index.tsx
+++ app/_common/nav/index.tsx
@@ -1,15 +1,38 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../../_assets/_images/logo.png'
 import S from './nav.module.css'
+import { useRouter } from 'next/navigation'
 
 //TODO: 사용자 프로필 이미지 가져오기
 function Nav() {
+  const router = useRouter()
+  const accessToken = localStorage.getItem('accessToken')
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken')
+    location.reload()
+  }
+  const handleLogin = () => {
+    router.push('/signin')
+  }
+
   return (
     <div className={S.nav}>
       <Link href="/main">
         <Image src={logo} alt="로고" width={100} height={100} />
       </Link>
+      {accessToken ? (
+        <button type="button" onClick={handleLogout}>
+          로그아웃
+        </button>
+      ) : (
+        <button type="button" onClick={handleLogin}>
+          로그인
+        </button>
+      )}
 
       <Link href="/mypage">
         <div className={S.userProfileWrapper}>
