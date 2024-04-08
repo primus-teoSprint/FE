e544deb507ffe6863a2350b6c68752c870c9dc3c
03hoho03 <ghwjd321@naver.com>
2024-04-08 20:59:51 +0900

    !!fix

--- app/_common/nav/index.tsx
+++ app/_common/nav/index.tsx
@@ -5,14 +5,25 @@ import Link from 'next/link'
 import logo from '../../_assets/_images/logo.png'
 import S from './nav.module.css'
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 //TODO: 사용자 프로필 이미지 가져오기
 function Nav() {
   const router = useRouter()
-  const accessToken = typeof window && localStorage?.getItem('accessToken')
+  const [isAuth, setIsAuth] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const item = localStorage.getItem('accessToken')
+      if (item) {
+        setIsAuth(true)
+      }
+    }
+  }, [])
 
   const handleLogout = () => {
     localStorage.removeItem('accessToken')
+    setIsAuth(false)
     location.reload()
   }
   const handleLogin = () => {
@@ -24,7 +35,7 @@ function Nav() {
       <Link href="/main">
         <Image src={logo} alt="로고" width={100} height={100} />
       </Link>
-      {accessToken ? (
+      {isAuth ? (
         <button className={S.btn} type="button" onClick={handleLogout}>
           로그아웃
         </button>
